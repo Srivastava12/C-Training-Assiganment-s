711a7dc4798834834492670b079a8c741702aaa1
Shubham Kumar <91357137+Srivastava12@users.noreply.github.com>
2024-09-12 21:01:06 +0530

    Add files via upload

---
+++ Question2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int a=0;
+	float b;
+	char c;
+	printf("Enter a  integer value :-");
+	scanf("%d",&a);
+	printf("Enter a  float value :-");
+	scanf("%f",&b);
+	printf("Enter a  char value :-");
+	scanf("%c",&c);
+	scanf("%c",&c);
+	printf("\n\nInteger value :- %d",a);
+	printf("\nFloat value :- %0.2f",b);
+	printf("\nchar value :- %c",c);
+}

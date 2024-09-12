e193438def7792263f6062ea2e880e2a557f9699
Shubham Kumar <91357137+Srivastava12@users.noreply.github.com>
2024-09-12 21:00:48 +0530

    Add files via upload

---
+++ Question1.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int  main(){
+	char name[50];
+	char dob[50];
+	char number[12];
+	long long int number1;
+	
+	printf("Name   :- John\n");
+	printf("DOB    :-August 15,1947\n");
+	printf("Mobile :-91-9999999999");
+
+	printf("\n\nEnter your Name :- ");
+	gets(name);
+	printf("Enter your DOB :- ");
+	scanf("%s",&dob);
+	printf("Enter your Number :- ");
+	scanf("%s",&number);
+
+	printf("\nName :- %s",name);
+	printf("\nDob :-%s",dob);
+	printf("\nNumber :-%s",number);
+	
+	printf("\n\nEnter your Name :- ");
+	gets(name);
+	gets(name);
+	printf("Enter your DOB :- ");
+	scanf("%s",&dob);
+	printf("Enter your Number :- ");
+	scanf("%lld",&number1);
+
+	printf("\nName :- %s",name);
+	printf("\nDob :-%s",dob);
+	printf("\nNumber :-%lld",number1);
+}

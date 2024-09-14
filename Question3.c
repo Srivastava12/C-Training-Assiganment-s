74831534bd0b7572b7f51cc2e30de032953261f4
Shubham Kumar <91357137+Srivastava12@users.noreply.github.com>
2024-09-14 09:52:44 +0530

    Update Question3.c

--- Question3.c
+++ Question3.c
@@ -1,3 +1,14 @@
+/*
+   Write a C program that accepts an employee's ID, total worked hours of a month and the amount he 
+   received per hour. Print the employee's ID and salary (with two decimal places) of a particular month.  
+    Test Data : 
+        Input the Employees ID:0342 
+        Input the working hrs: 8 
+        Salary amount/hr: 15000 
+        Expected Output: 
+        Employees ID = 0342 
+        Salary =  120000.00      
+*/
 #include<stdio.h>
 int main(){
 	char emp_id[10];

26fa1406f748c8953c1d8a2300e4d8c0e80d91fb
Shubham Kumar <91357137+Srivastava12@users.noreply.github.com>
2024-09-14 09:54:46 +0530

    Update Question5.c

--- Question5.c
+++ Question5.c
@@ -1,3 +1,9 @@
+/*
+    Write a C program that converts kilometers per hour to miles per hour. (1 KM = 0.621371 M) 
+    Expected Output : 
+        Input kilometers per hour: 15 
+        9.320568 miles per hour 
+*/
 #include<stdio.h>
 int main(){
 	//Declaring Variable

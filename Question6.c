134e99a5c677b95b919c179ebf1ebe47381bb806
Shubham Kumar <91357137+Srivastava12@users.noreply.github.com>
2024-09-14 09:55:28 +0530

    Update Question6.c

--- Question6.c
+++ Question6.c
@@ -1,3 +1,9 @@
+/*
+    Write a program in C that takes minutes as input, and display the total number of hours and minutes. 
+    Expected Output : 
+        Input minutes: 546 
+        9 Hours, 6 Minutes
+*/
 #include<stdio.h>
 int main(){
 	//Variable Declarating

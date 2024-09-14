7f621d1e5a9ff63afabb5efc12d204d84ed7bded
Shubham Kumar <91357137+Srivastava12@users.noreply.github.com>
2024-09-14 09:50:29 +0530

    Update Question1.c

--- Question1.c
+++ Question1.c
@@ -1,3 +1,11 @@
+/*
+    Write a C program to print your name, date of birth. and mobile number.  
+    Expected Output: 
+ 
+        Name   : John 
+        DOB      : August 15, 1947 
+        Mobile : 91-9999999999 
+*/
 #include<stdio.h>
 int  main(){
 	char name[50];
